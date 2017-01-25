9c515c2b3d136f49b0cb7a2b2e8e3f15f8b54dd8
mk46747 <matija.kurek@gmail.com>
2017-01-25 16:16:06 +0100

    m4

--- NannyApp.DAL/NHibernateService.cs
+++ NannyApp.DAL/NHibernateService.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NannyApp.DAL.Mappings;
+
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
 
 namespace NannyApp.DAL
 {
-    class NHibernateService
+
+    using global::NHibernate;
+    using global::NHibernate.Cfg;
+    using global::NHibernate.Tool.hbm2ddl;
+    public static class NHibernateService
     {
+        private static ISessionFactory sessionFactory;
+
+        public static ISession OpenSession()
+        {
+            try
+            {
+                if (sessionFactory == null)
+                {
+                    sessionFactory = OpenSessionFactory();
+                }
+                ISession session = sessionFactory.OpenSession();
+                return session;
+            }
+            catch (Exception e)
+            {
+                throw e.InnerException ?? e;
+            }
+        }
+
+        private static ISessionFactory OpenSessionFactory()
+        {
+            var nhConfig = Fluently.Configure()
+                .Database(SQLiteConfiguration.Standard
+                    .ConnectionString("Data Source=TestNHibernate_fluent.db;Version=3")
+                    .AdoNetBatchSize(100))
+                .Mappings(mappings => mappings.FluentMappings.AddFromAssemblyOf<UserMap>())
+                .BuildConfiguration();
+
+            var sessionFactory = nhConfig.BuildSessionFactory();
+
+            return sessionFactory;
+        }
     }
 }
---
+++ NannyApp.DAL/TestProgram.cs
@@ -0,0 +1,77 @@
+﻿using NannyApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NannyApp.DAL
+{
+    class TestProgram
+    {
+        private static void Main(string[] args)
+        {
+            CreateNannyAndSaveToDatabase();
+            //UpdateTobinAndAssignPierreAsManager();
+            //LoadEmployeesFromDatabase();
+        }
+
+        private static void CreateNannyAndSaveToDatabase()
+        {
+            var nanny = new Nanny(0, "nanny", "nanny");
+            nanny.Name = "Tobin Harris";
+
+            using (var session = NHibernateService.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    session.Save(nanny);
+                    transaction.Commit();
+                }
+                Console.WriteLine("Saved Nanny to the database");
+            }
+        }
+
+        private static void LoadEmployeesFromDatabase()
+        {
+            using (var session = NHibernateService.OpenSession())
+            {
+                var query = session.CreateQuery(
+                    "from Employee as emp order by emp.Name asc");
+
+                var foundNannys = query.List<Nanny>();
+
+                Console.WriteLine("\n{0} employees found:", foundNannys.Count);
+
+                foreach (var nanny in foundNannys)
+                {
+                    Console.WriteLine(nanny.Name);
+                }
+            }
+        }
+/*
+        private static void UpdateTobinAndAssignPierreAsManager()
+        {
+            using (var session = OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var q = session.CreateQuery("from Employee where Name = 'Tobin Harris'");
+
+                    var tobin = q.List<Employee>()[0];
+                    tobin.Name = "Tobin David Harris";
+
+                    var pierre = new Employee();
+                    pierre.Name = "Pierre Henri Kuate";
+
+                    tobin.Manager = pierre;
+                    session.Save(pierre);
+
+                    transaction.Commit();
+
+                    Console.WriteLine("Updated Tobin and added Pierre");
+                }
+            }
+        }*/
+    }
+}
