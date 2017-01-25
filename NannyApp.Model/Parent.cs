4950dafa1ce8c52a159262082ddfe6b880151665
mk46747 <matija.kurek@gmail.com>
2017-01-25 13:40:59 +0100

    Update model parent

---
+++ NannyApp.Model/Parent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NannyApp.Model
+{
+    public class Parent : User
+    {
+        public virtual IList<ParentOffer> Offers {get; set;}
+        public virtual IList<Cooperation> Cooperations {get; set;}
+        public Parent(int Id, string Username, string Password)
+            :base(Id, Username, Password)
+        {
+            
+        }
+        
+    }
+}
