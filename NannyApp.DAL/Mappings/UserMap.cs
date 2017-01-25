3691c423db08cb3bb81a1c1d18d4836b46003de8
mhlobik <marija.hlobik@gmail.com>
2017-01-25 14:18:31 +0100

    NannyApp.DAL create

---
+++ NannyApp.DAL/Mappings/UserMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NannyApp.Model;
+using FluentNHibernate.Mapping;
+
+namespace NannyApp.DAL.Mappings
+{
+    class UserMap : ClassMap<User>
+    {
+        public UserMap()
+        {
+            Table("User");
+            Id(b => b.Id).GeneratedBy.Native();
+            Map(b => b.Username).Not.Nullable().Length(50);
+            Map(b => b.Password).Not.Nullable().Length(50);
+            Map(b => b.Name).Not.Nullable().Length(50);
+            Map(b => b.Surname).Not.Nullable().Length(50);
+            Map(b => b.Gender).Not.Nullable().Length(10);
+            Map(b => b.Contact).Not.Nullable().Length(50);
+        }
+    }
+}
