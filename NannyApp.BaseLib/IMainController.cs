f38b99ca73a6b87650862eb6d7f1272087b759ab
mk46747 <matija.kurek@gmail.com>
2017-01-27 18:13:55 +0100

    nista bitno

---
+++ NannyApp.BaseLib/IMainController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NannyApp.BaseLib
+{
+    public interface IMainController
+    {
+        void AddNanny();
+        void AddParent();
+        void AddNannyOffer();
+        void AddParentOffer();
+
+       // void EditAccount(string accountName);
+    }
+}
