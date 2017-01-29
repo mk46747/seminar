dec7e9adf9bb52ef34d9cbbc3b9325268ab42dfe
mhlobik <marija.hlobik@gmail.com>
2017-01-29 23:56:53 +0100

    home/index

--- NannyApp/Controllers/HomeController.cs
+++ NannyApp/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using NannyApp.Model;
+using NannyApp.Models;
 using NannyApp.Model.Repositories;
 using NannyApp.Model.Factories;
 using NannyApp.DAL;
@@ -16,7 +17,26 @@ namespace NannyApp.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            OfferRepository offers = new OfferRepository();
+            List<OffersViewModel> offersList = new List<OffersViewModel>();
+            IList<NannyOffer> nannyOffers = offers.GetAllNannyOffers();
+            IList<ParentOffer> parentOffers = offers.GetAllParentOffers();
+            foreach (var nannyOffer in nannyOffers)
+            {
+                offersList.Add(new OffersViewModel
+                {
+                    NannyOffer = nannyOffer
+                });
+            }
+            foreach (var parentOffer in parentOffers)
+            {
+                offersList.Add(new OffersViewModel
+                {
+                    ParentOffer = parentOffer
+                });
+            }
+            return View(offersList.OrderByDescending(o => o.ParentOffer.Deadline).OrderByDescending(o => o.NannyOffer.Deadline));
+            //return View();
         }
 
 
---
+++ NannyApp/Models/OffersViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NannyApp.Model;
+using NannyApp.Model.Repositories;
+using NannyApp.Model.Factories;
+using NannyApp.DAL;
+using NannyApp.DAL.Repositories;
+using NHibernate;
+
+namespace NannyApp.Models
+{
+    public class OffersViewModel
+    {
+        public NannyOffer NannyOffer { get; set; }
+        public ParentOffer ParentOffer { get; set; }
+    }
+}
