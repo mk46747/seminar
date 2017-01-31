6c7ac7e5f1ecbba4a639d9d2d9a0ce2d985bed69
Laptop\Filip <f.soldan93@hotmail.com>
2017-01-31 18:09:22 +0100

    ble

--- NannyApp.Presentation/ReviewsForm.cs
+++ NannyApp.Presentation/ReviewsForm.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace NannyApp.Presentation
 {//
     public partial class ReviewsForm : Form
-    {
+    {//
         public ReviewsForm()
         {
             InitializeComponent();
